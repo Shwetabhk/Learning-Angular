e57e1d71dcd3933314edb49afe6459af2ed21d39
Shwetabh Kumar <shwetabh002@gmail.com>
2018-12-25 22:42:57 +0530

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],
